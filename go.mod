da2b4a52f21868747bba4913a2c70e5f9b5146d7
Kvertinum01 <mr_matvey444@mail.ru>
2022-05-27 22:29:21 +0300

    testing fasthttp

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/Kvertinum01/gomkpbot
 go 1.18
 
 require github.com/BurntSushi/toml v1.1.0
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.37.0 // indirect
+)
--- internal/app/vkapi/api.go
+++ internal/app/vkapi/api.go
@@ -2,9 +2,12 @@ package vkapi
 
 import (
 	"encoding/json"
-	"log"
-	"net/http"
-	"strconv"
+	"time"
+
+	// "log"
+	// "strconv"
+
+	"github.com/valyala/fasthttp"
 )
 
 const (
@@ -16,6 +19,7 @@ type Api struct {
 	Token   string
 	Url     string
 	Version string
+	Client  *fasthttp.Client
 }
 
 func NewApi(token string) *Api {
@@ -23,6 +27,11 @@ func NewApi(token string) *Api {
 		Token:   token,
 		Url:     baseApiUrl,
 		Version: baseApiVersion,
+		Client: &fasthttp.Client{
+			ReadTimeout:         5 * time.Second,
+			WriteTimeout:        5 * time.Second,
+			MaxIdleConnDuration: time.Minute,
+		},
 	}
 }
 
@@ -30,39 +39,59 @@ func (api *Api) Method(methodName string, params map[string]interface{}, respons
 	params["access_token"] = api.Token
 	params["v"] = api.Version
 
-	// urlParams := url.Values{}
-	strParams := "?"
-	for key, value := range params {
-		var strValue string
-		switch t := value.(type) {
-		case string:
-			strValue = value.(string)
-		case int:
-			strValue = strconv.Itoa(value.(int))
-		default:
-			byteValue, err := json.Marshal(value)
-			if err != nil {
-				log.Printf("Bad type %v\n", t)
-				return err
-			}
-			strValue = string(byteValue)
-		}
-		//urlParams.Add(key, strValue)
-		strParams += key + "=" + strValue + "&"
+	reqEntityBytes, err := json.Marshal(params)
+	if err != nil {
+		return err
 	}
+	reqUrl := api.Url + methodName
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
+
+	req.Header.SetMethod("POST")
+	req.Header.SetContentType("application/json")
+	req.SetRequestURI(reqUrl)
+	req.SetBody(reqEntityBytes)
+
+	api.Client.Do(req, resp)
+
+	return json.Unmarshal(resp.Body(), response)
+
+	// urlParams := url.Values{}
+	// strParams := "?"
+	// for key, value := range params {
+	// 	var strValue string
+	// 	switch t := value.(type) {
+	// 	case string:
+	// 		strValue = value.(string)
+	// 	case int:
+	// 		strValue = strconv.Itoa(value.(int))
+	// 	default:
+	// 		byteValue, err := json.Marshal(value)
+	// 		if err != nil {
+	// 			log.Printf("Bad type %v\n", t)
+	// 			return err
+	// 		}
+	// 		strValue = string(byteValue)
+	// 	}
+	// 	// urlParams.Add(key, strValue)
+	// 	strParams += key + "=" + strValue + "&"
+	// }
 
 	//apiAnswer, err := http.PostForm(api.Url+methodName, urlParams)
 
 	// Using GET method for better perfomance
-	apiAnswer, err := http.Get(api.Url + methodName + strParams)
-	if err != nil {
-		return err
-	}
-	defer apiAnswer.Body.Close()
+	//apiAnswer, err := http.Get(api.Url + methodName + strParams)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer apiAnswer.Body.Close()
 
-	if response == nil {
-		return nil
-	}
+	// if response == nil {
+	// 	return nil
+	// }
 
-	return json.NewDecoder(apiAnswer.Body).Decode(response)
+	// return json.NewDecoder(apiAnswer.Body).Decode(response)
 }
